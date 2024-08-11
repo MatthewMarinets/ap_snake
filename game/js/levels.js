b9f841c51893b80c1ac58027e96f4fc7b130e5ec
Matthew <matthew.marinets@gmail.com>
2024-08-11 14:45:17 -0700

    Adding levels 5 through 8

--- game/js/levels.js
+++ game/js/levels.js
@@ -11,7 +11,7 @@ const LEVEL_ONE_WALLS = [
     [24, 16, 1, 17],
 ];
 const LEVEL_ONE_WALL_COLOUR = [13/6, 9/16, 0.97];
-const LEVEL_ONE_EXIT = [16, 32, 0];
+const GENERAL_EXIT = [16, 32, 0];
 
 const LEVEL_TWO_WALLS = [
     // bounds
@@ -24,7 +24,6 @@ const LEVEL_TWO_WALLS = [
     [16, 16, 1, 17],
 ];
 const LEVEL_TWO_WALL_COLOUR = [13/6, 9/16, 0.97];
-const LEVEL_TWO_EXIT = [16, 32, 0];
 
 const LEVEL_THREE_WALLS = [
     // bounds
@@ -38,7 +37,6 @@ const LEVEL_THREE_WALLS = [
     [16, 24, 17, 1],
 ];
 const LEVEL_THREE_WALL_COLOUR = [13/6, 9/16, 0.97];
-const LEVEL_THREE_EXIT = [16, 32, 0];
 
 const LEVEL_FOUR_WALLS = [
     // bounds
@@ -55,11 +53,74 @@ const LEVEL_FOUR_WALLS = [
     [22, 8, 5, 1],
 ];
 const LEVEL_FOUR_WALL_COLOUR = [13/6, 9/16, 0.97];
-const LEVEL_FOUR_EXIT = [16, 32, 0];
+
+const LEVEL_FIVE_WALLS = [
+    // bounds
+    [16, 0, 33, 1],
+    [0, 16, 1, 33],
+    [16, 32, 33, 1],
+    [32, 16, 1, 33],
+    // L*?
+    [8, 16, 1, 17],
+    [24, 16, 1, 17],
+    [16, 16, 1, 1],
+    [20, 24, 9, 1],
+    [12, 8, 9, 1],
+];
+const LEVEL_FIVE_WALL_COLOUR = [13/6, 9/16, 0.97];
+
+const LEVEL_SIX_WALLS = [
+    // bounds
+    [16, 0, 33, 1],
+    [0, 16, 1, 33],
+    [16, 32, 33, 1],
+    [32, 16, 1, 33],
+    // || ||
+    [6, 16, 1, 23],
+    [9, 16, 1, 23],
+    [23, 16, 1, 23],
+    [26, 16, 1, 23],
+];
+const LEVEL_SIX_WALL_COLOUR = [13/6, 9/16, 0.97];
+
+const LEVEL_SEVEN_WALLS = [
+    // bounds
+    [16, 0, 33, 1],
+    [0, 16, 1, 33],
+    [16, 32, 33, 1],
+    [32, 16, 1, 33],
+    // T
+    [16, 27, 23, 1],
+    [9.5, 24, 10, 1],
+    [22.5, 24, 10, 1],
+    [14, 14.5, 1, 20],
+    [18, 14.5, 1, 20],
+];
+const LEVEL_SEVEN_WALL_COLOUR = [13/6, 9/16, 0.97];
+
+const LEVEL_EIGHT_WALLS = [
+    // bounds
+    [16, 0, 33, 1],
+    [0, 16, 1, 33],
+    [16, 32, 33, 1],
+    [32, 16, 1, 33],
+    // ||=||
+    [16, 27, 23, 1],
+    [16, 24, 23, 1],
+    [5, 10, 1, 21],
+    [8, 10, 1, 21],
+    [24, 10, 1, 21],
+    [27, 10, 1, 21],
+];
+const LEVEL_EIGHT_WALL_COLOUR = [13/6, 9/16, 0.97];
 
 export const LEVELS = [
-    {walls: LEVEL_ONE_WALLS, colour: LEVEL_ONE_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: LEVEL_ONE_EXIT},
-    {walls: LEVEL_TWO_WALLS, colour: LEVEL_TWO_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: LEVEL_TWO_EXIT},
-    {walls: LEVEL_THREE_WALLS, colour: LEVEL_THREE_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: LEVEL_THREE_EXIT},
-    {walls: LEVEL_FOUR_WALLS, colour: LEVEL_FOUR_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: LEVEL_FOUR_EXIT},
+    {walls: LEVEL_ONE_WALLS, colour: LEVEL_ONE_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: GENERAL_EXIT},
+    {walls: LEVEL_TWO_WALLS, colour: LEVEL_TWO_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: GENERAL_EXIT},
+    {walls: LEVEL_THREE_WALLS, colour: LEVEL_THREE_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: GENERAL_EXIT},
+    {walls: LEVEL_FOUR_WALLS, colour: LEVEL_FOUR_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: GENERAL_EXIT},
+    {walls: LEVEL_FIVE_WALLS, colour: LEVEL_FIVE_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: GENERAL_EXIT},
+    {walls: LEVEL_SIX_WALLS, colour: LEVEL_SIX_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: GENERAL_EXIT},
+    {walls: LEVEL_SEVEN_WALLS, colour: LEVEL_SEVEN_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: GENERAL_EXIT},
+    {walls: LEVEL_EIGHT_WALLS, colour: LEVEL_EIGHT_WALL_COLOUR, num_apples: 10, num_rounds: 2, exit: GENERAL_EXIT},
 ];
